1d5572e07a3414b837f368a888ba2a0241d6c5dc
Rodrigo Lucero <lucero.rodrigoar@gmail.com>
2019-05-11 20:10:05 -0300

    Estoy contento porque ya hice un inicio para la aplicacion, esta horrible el front pero bueno,ahora me voy a encargar de eso

--- app/src/main/java/com/example/appswim/MainActivity.kt
+++ app/src/main/java/com/example/appswim/MainActivity.kt
@@ -9,76 +9,39 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.widget.Button
 
 import com.example.appswim.database.SwimmerDao
 import com.example.appswim.database.SwimmerDatabase
+import com.example.appswim.SwimmerActivity
 import com.example.appswim.models.Swimmer
 
-
-import java.util.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
 
-    private var mSwimmerRecyclerView: RecyclerView? = null
-    private var mSwimmerRecyclerAdapter: SwimmerRecyclerAdapter? = null
-    private var mAddSwimmerFloatingActionButton: FloatingActionButton? = null
+    private var mAddSwimmerButton: Button? = null
     private var mSwimmerDao: SwimmerDao? = null
 
     private val RC_CREATE_SWIMMER = 1
-    private val RC_UPDATE_SWIMMER = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mSwimmerDao = Room.databaseBuilder(this, SwimmerDatabase::class.java, "db-swimmer")
+        mSwimmerDao = Room.databaseBuilder(this,
+                SwimmerDatabase::class.java, "db-swimmer")
                 .allowMainThreadQueries()
                 .build()
                 .swimmerDao
 
-        mSwimmerRecyclerView = findViewById(R.id.swimmerRecyclerView)
-        mSwimmerRecyclerView?.layoutManager = LinearLayoutManager(this)
-        mAddSwimmerFloatingActionButton = findViewById(R.id.addSwimmerFloatingActionButton)
+        mAddSwimmerButton = findViewById(R.id.addSwimmerButton)
 
-        val colors = intArrayOf(ContextCompat.getColor(this, R.color.colorAccent), ContextCompat.getColor(this, android.R.color.holo_red_light), ContextCompat.getColor(this, android.R.color.holo_orange_light), ContextCompat.getColor(this, android.R.color.holo_green_light), ContextCompat.getColor(this, android.R.color.holo_blue_dark), ContextCompat.getColor(this, android.R.color.holo_purple))
-
-        mSwimmerRecyclerAdapter = SwimmerRecyclerAdapter(this, ArrayList(), colors)
-        mSwimmerRecyclerAdapter?.addActionCallback(object : SwimmerRecyclerAdapter.ActionCallback {
-            override fun onLongClickListener(swimmer: Swimmer) {
-
-                val intent = Intent(this@MainActivity, UpdateSwimmerActivity::class.java)
-                intent.putExtra(UpdateSwimmerActivity.EXTRA_SWIMMER_ID, swimmer.id_swimmer)
-                startActivityForResult(intent, RC_UPDATE_SWIMMER)
-            }
-        })
-
-        mSwimmerRecyclerView?.setAdapter(mSwimmerRecyclerAdapter)
-
-        mAddSwimmerFloatingActionButton!!.setOnClickListener {
+        mAddSwimmerButton!!.setOnClickListener {
             val intent = Intent(this@MainActivity, CreateSwimmerActivity::class.java)
             startActivityForResult(intent, RC_CREATE_SWIMMER)
         }
-
-        loadSwimmers()
     }
 
-    private fun loadSwimmers() {
-        mSwimmerRecyclerAdapter!!.updateData(mSwimmerDao!!.swimmer)
-    }
-
-
-    /*
-    override fun onActivityResult(requestCode: Int, resultCode: Int
-
-    , data: Intent) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == RC_CREATE_SWIMMER && resultCode == Activity.RESULT_OK) {
-            loadSwimmers()
-        } else if (requestCode == RC_UPDATE_SWIMMER && resultCode == Activity.RESULT_OK) {
-            loadSwimmers()
-        }
-    }
-    */
 }
 
