eb621c120b8b28c66aaf8c6c5ba6a5d01b9c9136
Rodrigo Lucero <lucero.rodrigoar@gmail.com>
2019-05-14 20:30:07 -0300

    Agregamos a races en la base de datos

--- app/src/main/java/com/example/appswim/database/SwimmerDatabase.kt
+++ app/src/main/java/com/example/appswim/database/SwimmerDatabase.kt
@@ -2,10 +2,11 @@ package com.example.appswim.database
 
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
+import com.example.appswim.models.Races
 
 import com.example.appswim.models.Swimmer
 
-@Database(entities = [Swimmer::class], version = 4)
+@Database(entities = [Swimmer::class, Races::class], version = 4)
 abstract class SwimmerDatabase : RoomDatabase() {
     abstract val swimmerDao: SwimmerDao
 }
