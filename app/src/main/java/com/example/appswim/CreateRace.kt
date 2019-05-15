998b26fb287c4a818de89bb1685fe2cc2348c273
Rodrigo Lucero <lucero.rodrigoar@gmail.com>
2019-05-14 22:13:33 -0300

    Creando activity de crear carrera y su respectivo layout

---
+++ app/src/main/java/com/example/appswim/CreateRace.kt
@@ -0,0 +1,24 @@
+package com.example.appswim
+
+import android.support.v7.app.AppCompatActivity
+import android.widget.EditText
+import com.example.appswim.models.Races
+import com.example.appswim.models.Styles
+import java.util.*
+
+class create_races_activity : AppCompatActivity() {
+
+    private var mPlaceEditText: EditText? = null
+    private var mdateEditText: EditText? = null
+    private var mTimeNumberEditText: EditText? = null
+    private var mStyleEditText: EditText? = null
+
+}
+
+/*
+var id_race: Int = 0,
+var place: String,
+var date: Date,
+var time: Date,
+var Style: Styles
+*/
