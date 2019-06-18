3947a1ffa23a54ee2d0c4e615d4ddc77d42fb3e7
Rodrigo Lucero <lucero.rodrigoar@gmail.com>
2019-06-18 20:53:24 -0300

    Ni recuerdo que tenia aca, pero bueno, cualquier cosa vuelvo a algun commmit

--- app/src/main/java/com/example/appswim/CreateRace.kt
+++ app/src/main/java/com/example/appswim/CreateRace.kt
@@ -1,6 +1,13 @@
 package com.example.appswim
 
 import android.support.v7.app.AppCompatActivity
+import android.widget.EditText
 
 class CreateRace: AppCompatActivity() {
+
+    private var mDistanceEditText: EditText? = null
+    private var mTimeEditText: EditText? = null
+    private var mPlaceEditText: EditText? = null
+
+
 }
